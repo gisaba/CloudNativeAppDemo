1ee7d6f0d1127816ca0038764b57a97e382582d5
gisaba <gisaba1980@gmail.com>
2023-10-14 11:55:38 +0200

    DEMO CloudNativeApp
    Signed-off-by: gisaba <gisaba1980@gmail.com>

---
+++ TodoApi/TodoApi/Controllers/ProbeController.cs
@@ -0,0 +1,18 @@
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProbeController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("isAlive")]
+        public IActionResult isAlive()
+        {
+            return Ok("Todo API DEMO Version 1.0.0 Hostname:" + Dns.GetHostName());
+        }
+    }
+}
---
+++ TodoApi/TodoApi/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var aspnetcoreurl = Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls(aspnetcoreurl ?? "http://*:8080");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "TodoApi DEMO", Version = "v1" });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    app.UseSwagger(options =>
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    }));
+    app.UseSwaggerUI();
+} else
+{
+    app.UseSwagger(options =>
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    }));
+        app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
